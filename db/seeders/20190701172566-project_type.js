4b65f5a647b0361d13d6e080646f02a883607452
Walter Araya <naraya@fi.uba.ar>
2019-07-13 19:05:53 -0300

    Adds ids to projects types

--- db/seeders/20190701172566-project_type.js
+++ db/seeders/20190701172566-project_type.js
@@ -1,15 +1,18 @@
 export function up (queryInterface) {
   return queryInterface.bulkInsert('Project_types', [{
+    id: 1,
     name: 'TRABAJO_PROFESIONAL',
     created_at: new Date(),
     updated_at: new Date()
   },
   {
+    id: 2,
     name: 'TESIS',
     created_at: new Date(),
     updated_at: new Date()
   },
   {
+    id: 3,
     name: 'TRABAJO_PRACTICO',
     created_at: new Date(),
     updated_at: new Date()
