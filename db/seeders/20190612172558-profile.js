f9988be3a1a6228d477796fed13876de6f5fe0fb
Walter Araya <naraya@fi.uba.ar>
2019-07-13 14:39:47 -0300

    Adds ids to projects types

--- db/seeders/20190612172558-profile.js
+++ db/seeders/20190612172558-profile.js
@@ -1,30 +1,35 @@
 
 export function up (queryInterface) {
   return queryInterface.bulkInsert('Profiles', [{
+    id: 1,
     name: 'Admin',
     description: 'Administrador general',
     created_at: new Date(),
     updated_at: new Date()
   },
   {
+    id: 2,
     name: 'Estudiante',
     description: 'Estudiante FIUBA',
     created_at: new Date(),
     updated_at: new Date()
   },
   {
+    id: 3,
     name: 'Tutor',
     description: 'Docente Tutor FIUBA',
     created_at: new Date(),
     updated_at: new Date()
   },
   {
+    id: 4,
     name: 'Comision',
     description: 'Comision Curricular FIUBA',
     created_at: new Date(),
     updated_at: new Date()
   },
   {
+    id: 5,
     name: 'Interesado',
     description: 'Intermediario perfil externo',
     created_at: new Date(),
