9fde899baccc62f6cf4d62a4838ec0bda23f55aa
Sebastian Vicario <svicario@santandertecnologia.com.ar>
2020-10-03 18:24:13 -0300

    pido credentials de tutor para presentacion

--- modules/presentation/presentationRoute.js
+++ modules/presentation/presentationRoute.js
@@ -7,11 +7,11 @@ const multer = require('multer')
 const upload = multer({ dest: './uploads/' })
 
 const router = Router()
-router.post('/', createValidations, validateWithExpress, checkIsLoggedWithCredentials('APPROVE_PROJECTS'), validate(presentationController.createPresentation))
+router.post('/', createValidations, validateWithExpress, checkIsLoggedWithCredentials('EDIT_TUTOR_REQUESTS'), validate(presentationController.createPresentation))
 router.put('/:id([0-9]+)?/presentation/', upload.single('file'), validate(presentationController.uploadPresentation))
 router.put('/:id([0-9]+)?/documentation/', upload.single('file'), validate(presentationController.uploadDocumentation))
 router.put('/:id([0-9]+)?/', modifyValidations, validateWithExpress, checkIsLoggedWithCredentials('EDIT_PROJECTS'), validate(presentationController.putPresentation))
 router.get('/:id([0-9]+)?/', getValidations, validateWithExpress, checkIsLoggedWithCredentials('GET_PROJECTS'), validate(presentationController.getPresentation))
-router.put('/:id([0-9]+)?/submit/', getValidations, validateWithExpress, checkIsLoggedWithCredentials('EDIT_PROJECTS'), validate(presentationController.submitPresentation))
+router.put('/:id([0-9]+)?/submit/', getValidations, validateWithExpress, checkIsLoggedWithCredentials('EDIT_TUTOR_REQUESTS'), validate(presentationController.submitPresentation))
 
 export default router
